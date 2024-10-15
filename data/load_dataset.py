ec290b8dadaa1e4a42929945e18c268135c56875
geunsseo <geunsseo@naver.com>
2024-10-15 02:47:12 +0000

    fix: load_dataset.py 수정 - 불러오기와 병합 파트 분리

--- data/load_dataset.py
+++ data/load_dataset.py
@@ -1,7 +1,5 @@
 import os
 import pandas as pd
-import numpy as np
-from data.feature_engineering import find_nearest_haversine_distance
 
 def load_dataset():
     # 파일 경로 지정
@@ -18,90 +16,4 @@ def load_dataset():
     school_data: pd.DataFrame = pd.read_csv(os.path.join(data_path, "schoolinfo.csv"))
     park_data: pd.DataFrame = pd.read_csv(os.path.join(data_path, "parkInfo.csv"))
 
-    
-    ### 금리: 계약 연월 기준으로 interest_data를 train_data로 병합
-    train_data: pd.DataFrame = pd.merge(train_data, interest_data, left_on="contract_year_month", right_on="year_month", how="left")
-    train_data: pd.DataFrame = train_data.drop(columns=["year_month"])
-    
-    test_data: pd.DataFrame = pd.merge(test_data, interest_data, left_on="contract_year_month", right_on="year_month", how="left")
-    test_data: pd.DataFrame = test_data.drop(columns=["year_month"])
-
-    
-    ### 최단거리
-    # train_data에서 위도, 경도 중복 행을 제외하고 추출
-    unique_loc_train: pd.DataFrame = train_data[["latitude", "longitude"]].drop_duplicates().reset_index(drop=True)
-    unique_loc_test: pd.DataFrame = test_data[["latitude", "longitude"]].drop_duplicates().reset_index(drop=True)
-    
-    # train_data에 최단거리 지하철 정보 추가
-    merged_df: pd.DataFrame = find_nearest_haversine_distance(unique_loc_train, subway_data)
-    merged_df: pd.DataFrame = pd.concat([merged_df, unique_loc_train], axis=1)
-    train_data: pd.DataFrame = pd.merge(train_data, merged_df, on=["latitude", "longitude"], how="left")
-    train_data.rename(columns={
-        "nearest_distance": "nearest_subway_distance",
-        "nearest_latitude": "nearest_subway_latitude",
-        "nearest_longitude": "nearest_subway_longitude"
-        }, 
-        inplace=True
-    )
-
-    # test_data에 최단거리 지하철 정보 추가
-    merged_df: pd.DataFrame = find_nearest_haversine_distance(unique_loc_train, subway_data)
-    merged_df: pd.DataFrame = pd.concat([merged_df, unique_loc_train], axis=1)
-    test_data: pd.DataFrame = pd.merge(test_data, merged_df, on=["latitude", "longitude"], how="left")
-    test_data.rename(columns={
-        "nearest_distance": "nearest_subway_distance",
-        "nearest_latitude": "nearest_subway_latitude",
-        "nearest_longitude": "nearest_subway_longitude"
-        }, 
-        inplace=True
-    )
-
-    # train_data에 최단거리 학교 정보 추가
-    merged_df: pd.DataFrame = find_nearest_haversine_distance(unique_loc_train, school_data)
-    merged_df: pd.DataFrame = pd.concat([merged_df, unique_loc_train], axis=1)
-    train_data: pd.DataFrame = pd.merge(train_data, merged_df, on=["latitude", "longitude"], how="left")
-    train_data.rename(columns={
-        "nearest_distance": "nearest_school_distance",
-        "nearest_latitude": "nearest_school_latitude",
-        "nearest_longitude": "nearest_school_longitude"
-        },
-        inplace=True
-    )
-
-    # test_data에 최단거리 학교 정보 추가
-    merged_df: pd.DataFrame = find_nearest_haversine_distance(unique_loc_train, school_data)
-    merged_df: pd.DataFrame = pd.concat([merged_df, unique_loc_train], axis=1)
-    test_data: pd.DataFrame = pd.merge(test_data, merged_df, on=["latitude", "longitude"], how="left")
-    test_data.rename(columns={
-        "nearest_distance": "nearest_school_distance",
-        "nearest_latitude": "nearest_school_latitude",
-        "nearest_longitude": "nearest_school_longitude"
-        },
-        inplace=True
-    )
-
-    # train_data에 최단거리 공원 정보 추가
-    merged_df: pd.DataFrame = find_nearest_haversine_distance(unique_loc_train, park_data)
-    merged_df: pd.DataFrame = pd.concat([merged_df, unique_loc_train], axis=1)
-    train_data: pd.DataFrame = pd.merge(train_data, merged_df, on=["latitude", "longitude"], how="left")
-    train_data.rename(columns={
-        "nearest_distance": "nearest_park_distance",
-        "nearest_latitude": "nearest_park_latitude",
-        "nearest_longitude": "nearest_park_longitude"
-        },
-        inplace=True
-    )
-
-    # test_data에 최단거리 공원 정보 추가
-    merged_df: pd.DataFrame = find_nearest_haversine_distance(unique_loc_train, park_data)
-    merged_df: pd.DataFrame = pd.concat([merged_df, unique_loc_train], axis=1)
-    test_data: pd.DataFrame = pd.merge(test_data, merged_df, on=["latitude", "longitude"], how="left")
-    test_data.rename(columns={
-        "nearest_distance": "nearest_park_distance",
-        "nearest_latitude": "nearest_park_latitude",
-        "nearest_longitude": "nearest_park_longitude"
-        },
-        inplace=True
-    )
-
-    return train_data, test_data, sample_submission
+    return train_data, test_data, sample_submission, interest_data, subway_data, school_data, park_data
