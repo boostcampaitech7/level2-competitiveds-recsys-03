092e8dcf02312ce4312ab9a5cdced14d0508e9af
geunsseo <geunsseo@naver.com>
2024-10-16 06:41:16 +0000

    feat: data_preprocessing.py에 delete_low_density 함수 추가 - 위치 중복도 선택/제거 함수

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -16,4 +16,43 @@ def outlier_detection(data: pd.Series) -> pd.Series:
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
 
-    return data[(data < lower_bound) | (data > upper_bound)]
+
+# 위치 중복도 선택/제거 함수
+def delete_low_density(
+    data: pd.DataFrame, 
+    lower: int, 
+    upper: int,
+    drop: bool = True
+) -> pd.DataFrame:
+    """위치 중복도가 선택할 구간에 포함되면 이에 해당하는 데이터를 반환하거나 또는 해당하는 데이터를 제거하는 함수
+
+    Args:
+        data (pd.DataFrame): 위도, 경도를 column으로 갖는 데이터프레임
+        lower (int): 선택할 구간의 하한
+        upper (int): 선택할 구간의 상한
+        drop (bool, optional): False이면 구간에 해당하는 데이터 반환. Default는 True.
+
+    Returns:
+        pd.DataFrame: 선택할 구간에 포함하는 위치 중복도를 갖는 데이터를 선택 또는 삭제 후 데이터프레임 반환
+    """
+
+    group = data.groupby(["latitude", "longitude"])["index"].count() # 위도, 경도 그룹별 개수 카운트
+    drop_index = group[(group >= lower) & (group <= upper)].index # 구간에 해당하는 위도, 경도 중복을 갖는 데이터 인덱스 후보
+    
+    # 조건에 해당하는 데이터프레임 저장
+    if drop == True:
+        result_df = train_data[
+            ~ (train_data["latitude"].isin(drop_index.get_level_values(0))
+            & train_data["longitude"].isin(drop_index.get_level_values(1)))
+        ].reset_index()
+        result_df.drop(columns="level_0", inplace=True) # 필요없는 column 제거
+        result_df["index"] = result_df.index # "index" column도 초기화
+    else:
+        result_df = train_data[
+              (train_data["latitude"].isin(drop_index.get_level_values(0))
+            & train_data["longitude"].isin(drop_index.get_level_values(1)))
+        ].reset_index()
+        result_df.drop(columns="level_0", inplace=True) # 필요없는 column 제거
+        result_df["index"] = result_df.index # # "index" column도 초기화
+
+    return result_df
