221beb3c48ea8164d155b02d2f62a593592054c8
TaroSin <twokst@naver.com>
2024-10-16 18:55:47 +0900

    fix: feature_select.py doc string 추가

--- model/feature_select.py
+++ model/feature_select.py
@@ -1,4 +1,16 @@
 def select_features(train_data, test_data):
+    """
+    학습 데이터와 테스트 데이터에서 사용할 피처(컬럼)를 선택하는 함수.
+
+    Args:
+        train_data (DataFrame): 학습에 사용할 데이터프레임.
+        test_data (DataFrame): 테스트에 사용할 데이터프레임.
+
+    Returns:
+        tuple:
+            - train_data_selected (DataFrame): 선택된 피처들로 구성된 학습 데이터.
+            - test_data_selected (DataFrame): 선택된 피처들로 구성된 테스트 데이터.
+    """
     # 학습 데이터에서 사용할 피처들
     train_cols = [
         "deposit",
